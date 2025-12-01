766d09db4101cb7422c291d4419dd8771dbd76f0
CoderKashyap <kashyap.abhi1105@gmail.com>
2025-12-01 22:55:51 +0530

    hide trusted brand section

--- app/page.js
+++ app/page.js
@@ -29,7 +29,7 @@ export default function Home() {
         {/* <IntegrationTools /> */}
         {/* <TechStackExpertise /> */}
 
-        <TrustedBrands />
+        {/* <TrustedBrands /> */}
 
         <LisaCta />
         <CtaSection />
