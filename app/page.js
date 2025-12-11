7484f9855a417ae13bff2c61aa9d5227f971d1de
CoderKashyap <kashyap.abhi1105@gmail.com>
2025-12-11 14:12:55 +0530

    add new integration tools section

--- app/page.js
+++ app/page.js
@@ -3,16 +3,13 @@
 import { Header } from "@/components/home/header"
 import { HeroSection } from "@/components/home/heroSection"
 import { AnimatedShowcase } from "@/components/home/animated-showcase"
-// import { TrustedBrands } from "@/components/home/trusted-brands"
 import { CtaSection } from "@/components/home/cta-section"
 import { TabbedShowcase } from "@/components/home/tabbed-showcase"
-// import StatsSection from "@/components/home/stats-section"
-import LisaCta from "@/components/home/lisaCta"
 import Stats3D from "@/components/home/StatsBarSection"
-import TechStackExpertise from "@/components/home/TechStackExpertise"
-import { IntegrationsSection } from "@/components/home/IntegrationsSection"
+import IntegrationsSection from "@/components/home/IntegrationsSection"
 import VideoHeroSection from "@/components/videoHero"
-// import IntegrationsSection from "@/components/home/IntegrationsSection"
+import IntegrationOrbit from "@/components/home/IntegrationOrbit"
+// import { TrustedBrands } from "@/components/home/trusted-brands"
 
 export default function Home() {
 
@@ -27,8 +24,6 @@ export default function Home() {
         <VideoHeroSection />
 
         <TabbedShowcase />
-
-        {/* <TechStackExpertise /> */}
 
         <IntegrationsSection />
 
