ffc9e0b633c77043ab4a7567b40ba2aba0919140
CoderKashyap <kashyap.abhi1105@gmail.com>
2025-12-31 23:07:36 +0530

    footer update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,8 @@
 import Link from "next/link"
 
 import { Linkedin, Facebook, Youtube, Instagram } from 'lucide-react'
-import whiteLogo from '@/public/logo/TaygoLogoWhite.svg'
-import whiteLogoRound from '@/public/logo/whiteRoundLogo.png'
+import taygoLogo from '@/public/logo/taygoLogo.png'
+import taygoIcon from '@/public/logo/taygoIcon.png'
 import Image from "next/image"
 import { FaXTwitter } from "react-icons/fa6";
 
@@ -14,38 +14,38 @@ const Footer = () => {
     return (
         <>
 
-            <footer className="bg-[#2b3754] text-gray-100">
+            <footer>
 
 
                 <div className="sm:py-14 py-8">
-                    <div className="container mx-auto px-6 sm:px-24 md:px-28">
+                    <div className="container mx-auto px-6 sm:px-10">
 
                         <div className="md:px-6 px-2 pt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8 md:gap-16">
 
 
-                            <div className="space-y-8 col-span-2">
+                            <div className="space-y-8 col-span-2"> 
                                 <div>
-                                    <Link href="/"> <Image src={whiteLogo} alt="Taygo Logo" className="w-32 h-auto mb-4" /> </Link>
-                                    <p className="text-md text-gray-300">Ultimate productivity by automating Lead Management, Call System, Web Presence and Customer Communication with TAYGO™.</p>
+                                    <Link href="/"> <Image src={taygoLogo} alt="Taygo Logo" className="w-38 h-auto mb-6" /> </Link>
+                                    <p className="text-gray-700 text-xl hover:text-gray-800 font-medium">Ultimate productivity by automating Lead Management, Call System, Web Presence and Customer Communication with TAYGO™.</p>
                                 </div>
 
                                 <div>
                                     {/* <h4 className="font-semibold text-lg mb-4">Social Media</h4> */}
 
                                     <div className="flex gap-6 my-4 md:mb-0">
-                                        <Link href="https://www.facebook.com/taygohomeloan" className="text-gray-300 hover:text-white">
+                                        <Link href="https://www.facebook.com/taygohomeloan" className="text-gray-700 hover:text-gray-800 font-medium">
                                             <Facebook className="w-6 h-6" />
                                         </Link>
-                                        <Link href="https://x.com/taygoinc" className="text-gray-300 hover:text-white">
+                                        <Link href="https://x.com/taygoinc" className="text-gray-700 hover:text-gray-800 font-medium">
                                             <FaXTwitter className="w-6 h-6" />
                                         </Link>
-                                        {/* <Link href="#" className="text-gray-300 hover:text-white">
+                                        {/* <Link href="#" className="text-gray-700 hover:text-gray-800 font-medium">
                                             <Youtube className="w-6 h-6" />
                                         </Link> */}
-                                        <Link href="https://www.linkedin.com/company/taygo/" className="text-gray-300 hover:text-white">
+                                        <Link href="https://www.linkedin.com/company/taygo/" className="text-gray-700 hover:text-gray-800 font-medium">
                                             <Linkedin className="w-6 h-6" />
                                         </Link>
-                                        <Link href="https://www.instagram.com/taygo.inc/" className="text-gray-300 hover:text-white">
+                                        <Link href="https://www.instagram.com/taygo.inc/" className="text-gray-700 hover:text-gray-800 font-medium">
                                             <Instagram className="w-6 h-6" />
                                         </Link>
                                     </div>
@@ -58,23 +58,23 @@ const Footer = () => {
 
                             {/* Digital Transformation Section */}
                             <div>
-                                <h3 className="text-lg font-semibold mb-4">On Our Site</h3>
-                                <ul className="space-y-2 text-md">
-                                    <li><Link href="https://www.taygo.com/bn/app/contactus-y7f" className="text-gray-300 hover:text-white">Contact Us</Link></li>
-                                    {/* <li><Link href="#" className="text-gray-300 hover:text-white">Support</Link></li> */}
-                                    <li><Link href="https://cloud.taygo.com/terms-of-services" className="text-gray-300 hover:text-white">Terms of service</Link></li>
-                                    <li><Link href="https://cloud.taygo.com/privacy-policy" className="text-gray-300 hover:text-white">Privacy Policy</Link></li>
+                                <h3 className="text-gray-800 text-2xl font-semibold mb-4">On Our Site</h3>
+                                <ul className="space-y-4 text-xl">
+                                    <li><Link href="https://www.taygo.com/bn/app/contactus-y7f" className="text-gray-700 hover:text-gray-800 font-medium">Contact Us</Link></li>
+                                    {/* <li><Link href="#" className="text-gray-700 hover:text-gray-800 font-medium">Support</Link></li> */}
+                                    <li><Link href="https://cloud.taygo.com/terms-of-services" className="text-gray-700 hover:text-gray-800 font-medium">Terms of service</Link></li>
+                                    <li><Link href="https://cloud.taygo.com/privacy-policy" className="text-gray-700 hover:text-gray-800 font-medium">Privacy Policy</Link></li>
 
                                 </ul>
                             </div>
 
                             {/* Resources */}
                             <div className='sm:ml-12 ml-0'>
-                                <h3 className="text-lg font-semibold mb-4">Resources</h3>
-                                <ul className="space-y-2 text-md">
-                                    <li><Link href="https://cloud.taygo.com/features" className="text-gray-300 hover:text-white">Features</Link></li>
-                                    <li><Link href="https://cloud.taygo.com/pricing" className="text-gray-300 hover:text-white">Pricing</Link></li>
-                                    <li><Link href="#" className="text-gray-300 hover:text-white">Blogs</Link></li>
+                                <h3 className="text-gray-800 text-2xl font-semibold mb-4">Resources</h3>
+                                <ul className="space-y-4 text-xl">
+                                    <li><Link href="https://cloud.taygo.com/features" className="text-gray-700 hover:text-gray-800 font-medium">Features</Link></li>
+                                    <li><Link href="https://cloud.taygo.com/pricing" className="text-gray-700 hover:text-gray-800 font-medium">Pricing</Link></li>
+                                    <li><Link href="#" className="text-gray-700 hover:text-gray-800 font-medium">Blogs</Link></li>
                                 </ul>
                             </div>
 
@@ -82,10 +82,10 @@ const Footer = () => {
                             <div className='space-y-8'>
 
                                 <div>
-                                    <h3 className="text-lg font-semibold mb-4">Contact</h3>
-                                    <ul className="space-y-2 text-md">
-                                        <li><Link href="mailto:taygo@taygo.com" className="text-gray-100 hover:text-white">taygo@taygo.com</Link></li>
-                                        <li><div className="text-gray-100 hover:text-white">Silicon Valley, CA</div></li>
+                                    <h3 className="text-gray-800 text-2xl font-semibold mb-4">Contact</h3>
+                                    <ul className="space-y-4 text-xl">
+                                        <li><Link href="mailto:taygo@taygo.com" className="text-gray-700 hover:text-gray-800 font-medium">taygo@taygo.com</Link></li>
+                                        <li><div className="text-gray-700 hover:text-gray-800 font-medium">Silicon Valley, CA</div></li>
                                     </ul>
                                 </div>
 
@@ -95,11 +95,11 @@ const Footer = () => {
 
 
                         {/* Bottom Section */}
-                        <div className="mt-8 pt-3 text-md">
+                        <div className="mt-8 pt-3 text-xl">
                             <div className="">
 
-                                <div className="flex justify-center items-center text-gray-100 hover:text-white gap-3">
-                                    <Image src={whiteLogoRound} alt="Taygo Logo" height={38} width={36} />
+                                <div className="flex justify-center items-center text-gray-700 hover:text-gray-800 gap-3">
+                                    <Image src={taygoIcon} alt="Taygo Logo" height={38} width={36} />
                                     ©{new Date().getFullYear()} TAYGO™.  All Rights Reserved.
                                 </div>
                             </div>
